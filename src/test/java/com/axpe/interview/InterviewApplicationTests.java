12ae3ca2c6d6c47ea1d48908ec8c0ef337a77e28
Agustín Varela <agus.varela10@gmail.com>
2024-02-21 21:57:17 +0100

    First commit

---
+++ src/test/java/com/axpe/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axpe.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
